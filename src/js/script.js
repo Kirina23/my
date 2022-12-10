48461a756f64ffc9e7242c151620743095c6b819
Irina Kirichenko <kira.airis23@gmail.com>
2022-12-10 22:34:09 +0600

    animation

--- src/js/script.js
+++ src/js/script.js
@@ -48,4 +48,68 @@ $(document).ready(function(){
          $('.overlay, #order').fadeIn('slow');
       })
    })
+   
+   function valideForms(form) {
+   $(form).validate( {
+      rules: {
+         name: {
+            required: true,
+            minlength: 2
+         },
+         phone: "required",
+         email: {
+            required: true,
+            email: true
+         }
+         },
+         messages: {
+            name: {
+            required:   "Пожалуйста введите свое имя",
+            minlength: jQuery.validator.format("Введите не менее {0} символов!")
+            },
+            phone: "Пожалуйста введите свой телефон",
+            email: {
+               required: "Нам нужен правильный email для связи с вами",
+               email: "email должен быть в формате: name@domain.com"
+         }
+
+      }
+   }); 
+   }
+   valideForms('#choice');
+   valideForms('#consultation form');
+   valideForms('#order form');
+
+   $('input[name="phone"]').mask("+7 (999) 999-99-99");
+
+
+   $('form').submit(function(e) {
+      e.preventDefault();
+
+      if (!$(this).valid()) {
+         return;
+      }
+
+      $.ajax({
+         type: "POST",
+         url: "mailer/smart.php",
+         data: $(this).serialize()
+      }).done(function() {
+         $(this).find("input").val("");
+         $('#consultation, #order').fadeOut()
+         $('overlay, #thanks').fadeIn('slow')
+         $('form').trigger('reset');
+      });
+      return false;
+   });
+
+//Smooth scroll and page up
+   $(window).scroll(function() {
+      if ($(this).scrollTop() > 1600) {
+         $('.pageup').fadeIn();
+      } else {
+         $('.pageup').fadeOut();   
+      }
+   });
+   
 });
