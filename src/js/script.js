624af8281c8bdb4171c6ba4354121cb9c6cafad8
Irina Kirichenko <kira.airis23@gmail.com>
2022-12-11 09:56:43 +0600

    correction mistakes

--- src/js/script.js
+++ src/js/script.js
@@ -106,10 +106,18 @@ $(document).ready(function(){
 //Smooth scroll and page up
    $(window).scroll(function() {
       if ($(this).scrollTop() > 1600) {
-         $('.pageup').fadeIn();
+            $('.pageup').fadeIn();
       } else {
-         $('.pageup').fadeOut();   
+            $('.pageup').fadeOut();
       }
    });
+
+   $("a[href^='#']").click(function(){
+      const _href = $(this).attr("href");
+      $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+      return false;
+   });
    
+
+   new WOW().init();
 });
